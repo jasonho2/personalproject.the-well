028e13969ebc8a8e8a982b1f983927c12652b2cf
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-05-16 08:31:00 -0700

    Add name to admins, update post cards

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts or /posts.json
@@ -33,14 +34,11 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path, notice: "Post updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_16_005700) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_16_151351) do
   create_table "members", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_16_005700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
